4fa89ad154a8e3741643b6f6fbedd13e7297fe1f
Cheyoon Jung <jessietwo94@gmail.com>
2022-04-29 17:42:42 +0200

    learning

--- react-kclass/react-prac/src/App.js
+++ react-kclass/react-prac/src/App.js
@@ -2,14 +2,44 @@ import "./App.css";
 import { useState } from "react";
 
 function App() {
-  const [counter, setCounter] = useState(0);
-  const onClick = () => {
-    setCounter(counter + 1);
+  const [minutes, setMinutes] = useState(0);
+  const [flipped, setFlipped] = useState(0);
+  const onChange = (e) => {
+    setMinutes(e.target.value);
   };
+  const reset = () => {
+    return setMinutes(0);
+  };
+  const onFlip = () => {
+    return setFlipped(!flipped);
+  };
+
   return (
     <>
-      <h3>Total Clicked:{counter}</h3>
-      <button onClick={onClick}>Click me</button>
+      <div>
+        <h1>Super converter</h1>
+        <label htmlFor="minutes">Minutes</label>
+        <input
+          value={minutes}
+          id="minutes"
+          placeholder="Minutes"
+          type="number"
+          onChange={onChange}
+        />
+        <h4>You want to convert {minutes}</h4>
+      </div>
+      <div>
+        <label htmlFor="hours">Hours</label>
+        <input
+          value={Math.round(minutes / 60)}
+          id="hours"
+          placeholder="Hours"
+          type="number"
+          disabled
+        />
+      </div>
+      <button onClick={reset}>Rest</button>
+      <button onClick={onFlip}>Flipped</button>
     </>
   );
 }
