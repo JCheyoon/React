6ec481370758fe15f0dae1eef182d1332e4cbe98
Cheyoon Jung <jessietwo94@gmail.com>
2022-05-05 13:04:34 +0200

    learning

--- react-kclass/react-prac/src/App.js
+++ react-kclass/react-prac/src/App.js
@@ -1,14 +1,30 @@
 import "./App.css";
 import Btn from "./components/Btn";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import * as PropTypes from "prop-types";
 
 function App() {
-  const [value, setValue] = useState("Save Changes");
-  const changeValue = () => setValue("Revert Change");
+  const [counter, setCounter] = useState(0);
+  const [keyword, setKeyword] = useState("");
+  const onClick = () => setCounter((prev) => prev + 1);
+  const onChange = (e) => setKeyword(e.target.value);
+
+  console.log("I run all the time");
+  const runOnlyOnce = () => {
+    console.log("I run only once");
+  };
+  useEffect(runOnlyOnce, []);
+
   return (
     <>
-      <Btn text={value} changeValue={changeValue} />
-      <Btn text="Confirm" />
+      <input
+        value={keyword}
+        onChange={onChange}
+        type="text"
+        placeholder="Search Here"
+      />
+      <h1>{counter}</h1>
+      <button onClick={onClick}>Click me!</button>
     </>
   );
 }
--- react-kclass/react-prac/src/components/Btn.js
+++ react-kclass/react-prac/src/components/Btn.js
@@ -1,16 +1,7 @@
+import styles from "./Btn.module.css";
 function Btn({ text, changeValue }) {
   return (
-    <button
-      onClick={changeValue}
-      style={{
-        backgroundColor: "tomato",
-        color: "white",
-        padding: "10px 2px",
-        border: 0,
-        borderRadius: 10,
-        cursor: "pointer",
-      }}
-    >
+    <button onClick={changeValue} className={styles.Btn}>
       {text}
     </button>
   );
