98bc6e202cbe5d4b6df2ffa66c636cebe61ad6f1
Cheyoon Jung <jessietwo94@gmail.com>
2022-05-11 11:09:04 +0200

    learning

--- react-kclass/react-prac/src/App.js
+++ react-kclass/react-prac/src/App.js
@@ -1,35 +1,9 @@
 import "./App.css";
 import { useState, useEffect } from "react";
-import * as PropTypes from "prop-types";
+import { logDOM } from "@testing-library/react";
 
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [coins, setCoins] = useState([]);
-  useEffect(() => {
-    fetch("https://api.coinpaprika.com/v1/tickers")
-      .then((res) => res.json())
-      .then((data) => {
-        setCoins(data);
-        setLoading(false);
-      });
-  }, []);
-
-  return (
-    <>
-      <h1>The coins! {loading ? "" : `(${coins.length})`}</h1>
-      {loading ? (
-        <strong>Loading....</strong>
-      ) : (
-        <select>
-          {coins.map((coin) => (
-            <option key={coin.id}>
-              {coin.name}({coin.symbol}) :${coin.quotes.USD.price}
-            </option>
-          ))}
-        </select>
-      )}
-    </>
-  );
+  return null;
 }
 
 export default App;
