9b8477e4641cea41b6d923f105d9d898a82276e5
Cheyoon Jung <jessietwo94@gmail.com>
2022-06-03 10:12:54 +0200

    learning

--- shopping-site/src/App.js
+++ shopping-site/src/App.js
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./Routes/Home/Home.component";
 import Navigation from "./Routes/Navigation/Navigation.componet";
+import SignIn from "./Sign-in/Sign-in.componet";
 
 const Shop = () => {
   return <h1>TEST</h1>;
@@ -12,6 +13,7 @@ function App() {
       <Route Path="/" element={<Navigation />}>
         <Route index element={<Home />} />
         <Route path="shop" element={<Shop />} />
+        <Route path="signIn" element={<SignIn />} />
       </Route>
     </Routes>
   );
