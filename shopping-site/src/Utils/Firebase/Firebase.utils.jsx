1b58a23667823116f4e5e730db810165e70603cb
Cheyoon Jung <jessietwo94@gmail.com>
2022-06-08 11:45:40 +0200

    update

--- shopping-site/src/Utils/Firebase/Firebase.utils.jsx
+++ shopping-site/src/Utils/Firebase/Firebase.utils.jsx
@@ -6,7 +6,15 @@ import {
   GoogleAuthProvider,
 } from "firebase/auth";
 import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
-import { firebaseConfig } from "../../../firebase-config";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBrVfXrG1yZvmPl85gXqo-dIX4-tUG06ik",
+  authDomain: "jc-crown-shopping-db.firebaseapp.com",
+  projectId: "jc-crown-shopping-db",
+  storageBucket: "jc-crown-shopping-db.appspot.com",
+  messagingSenderId: "969389879327",
+  appId: "1:969389879327:web:be706e614775ecbc6f252a",
+};
 
 // Initialize Firebase
 const firebaseApp = initializeApp(firebaseConfig);
