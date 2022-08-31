075e5cab3c140df0c761b5a52280d2ebb2632939
Cheyoon Jung <jessietwo94@gmail.com>
2022-08-31 09:03:35 +0200

    learning

---
+++ redux/src/App.js
@@ -0,0 +1,12 @@
+import Counter from "./components/Counter";
+import { Provider } from "react-redux";
+
+function App() {
+  return (
+    <Provider store={store}>
+      <Counter />
+    </Provider>
+  );
+}
+
+export default App;
